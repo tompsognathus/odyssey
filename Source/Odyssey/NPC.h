056651d898f550477edcc7632530fd6d97a783bf
tompsognathus <tompsognathus@gmail.com>
2023-07-10 16:01:40 +0100

    NPCs now have prompt widgets to interact

---
+++ Source/Odyssey/NPC.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "Interactable.h"
+
+#include "NPC.generated.h"
+
+UCLASS()
+class ODYSSEY_API ANPC : public AActor, public IInteractable
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ANPC();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
