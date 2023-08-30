d0c30bf91b63e0a8401dfbc17e4660b9a186d8a9
tompsognathus <tompsognathus@gmail.com>
2023-08-30 13:21:51 +0100

    added memories widget blueprint

---
+++ Source/Odyssey/MemoriesWidget.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MemoriesWidget.generated.h"
+
+
+UCLASS()
+class ODYSSEY_API UMemoriesWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
